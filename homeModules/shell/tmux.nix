09b2cd8eff908d1516dc713ec7ca62cf652f5b13
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-06-13 16:58:16 +0200

    tmux has been added

--- homeModules/shell/tmux.nix
+++ homeModules/shell/tmux.nix
@@ -6,11 +6,13 @@
 
     plugins = with pkgs.tmuxPlugins; [
       catppuccin
+      cpu
+      battery
     ];
 
     extraConfig = ''
       unbind r
-      bind r source-file ~/.tmux.conf
+      bind r source-file ~/.config/tmux/tmux.conf
 
       set -g prefix C-z
 
@@ -19,10 +21,25 @@
       bind-key k select-pane -U
       bind-key l select-pane -R
 
-      set -g @plugin 'tmux-plugins/tpm'
-      set -g @plugin 'catppuccin/tpm'
+      set-option -g status-position top
 
-      run '~/.tmux/plugins/tpm/tpm'
+      set -g @catppuccin_window_left_separator "█"
+      set -g @catppuccin_window_right_separator "█ "
+      set -g @catppuccin_window_number_position "right"
+      set -g @catppuccin_window_middle_separator "  █"
+
+      set -g @catppuccin_window_default_fill "number"
+
+      set -g @catppuccin_window_current_fill "number"
+      set -g @catppuccin_window_current_text "#{pane_current_path}"
+
+      set -g @catppuccin_status_modules_right "application session date_time"
+      set -g @catppuccin_status_left_separator  ""
+      set -g @catppuccin_status_right_separator " "
+      set -g @catppuccin_status_right_separator_inverse "yes"
+      set -g @catppuccin_status_fill "all"
+      set -g @catppuccin_status_connect_separator "no"
+
       '';
 
   };
