05c3ef7590fb56422930bc8640ac6e98b3234a12
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-09-25 14:49:59 +0200

    spliting my conig

--- hosts/default/configuration.nix
+++ hosts/default/configuration.nix
@@ -161,6 +161,8 @@
       rustup
       spotify
       playerctl
+      android-studio
+      unzip
     ];
   };
 
@@ -184,36 +186,13 @@
     };
   };
 
-  # Install firefox.
   programs.firefox.enable = true;
   programs.zsh.enable = true;
   programs.thunar.enable = true;
 
-  # Allow unfree packages
   nixpkgs.config.allowUnfree = true;
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  environment.systemPackages = with pkgs; [
-     vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-     bluez
-     blueman
-     home-manager
-     os-prober  
-     git
-     fastfetch
-     rofi
-     swww
-     megacmd
-     sddm-astronaut
-     brightnessctl
-     bun
-     wl-clipboard
-     grim
-    #  npm
-     nodejs
-    banana-cursor
-  ];
+ 
 
   system.stateVersion = "25.05"; # Did you read the comment?
 
---
+++ systemModules/miscPack.nix
@@ -0,0 +1,27 @@
+{ pkgs, ...}: 
+
+{ 
+ environment.systemPackages = with pkgs; [
+    vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+    bluez
+    blueman
+    home-manager
+    os-prober  
+    git
+    fastfetch
+    rofi
+    swww
+    megacmd
+    sddm-astronaut
+    brightnessctl
+    bun
+    wl-clipboard
+    grim
+    nodejs
+    banana-cursor
+    kotlin
+    kotlin-language-server
+    kotlin-native
+    java
+  ];
+}
