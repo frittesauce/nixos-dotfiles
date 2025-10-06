1aa96566c8b47f302fa3bc5d1962972be2aa7679
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-10-06 12:37:39 +0200

    neovim

--- modules/system/code/nvim.nix
+++ modules/system/code/nvim.nix
@@ -3,5 +3,22 @@
 { 
  environment.systemPackages = with pkgs; [
    neovim 
+   tree-sitter
+   curl
+   fzf
+   ripgrep
+   fd
+
+   python3
+   stylua
+   ast-grep
+   lua
+   unzip
+   wget
+   go
+   composer
+   php 
+   julia
+   pip
  ];
 }
