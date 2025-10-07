30ce7f4d4b1c1b68a40ab5e9ad58cb0eff700064
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-10-07 18:22:34 +0200

    fixed

--- rebuild.sh
+++ rebuild.sh
@@ -1,10 +1,12 @@
 #!/usr/bin/env bash
 echo "rebuilding!!"
 
-CURRENT_THEME=$(cat ~/.config/themeses/current_theme)
+CURRENT_THEME=$(cat ~/nixos/current_theme)
 
 echo "theme $CURRENT_THEME is selected"
 
 sudo nixos-rebuild switch --flake /home/farmslop/nixos/#desktop
 find ~/nixos/dotfiles/ -type f -name "*.sh" -exec chmod +x {} \;
+
 stow -R dotfiles
+switch_theme.sh "$CURRENT_THEME"
