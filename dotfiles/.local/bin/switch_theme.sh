edbfdb9a2b0af78bd98a57a59e2662600bf49847
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-10-07 20:16:18 +0200

    temp purple wallpaper

--- dotfiles/.local/bin/switch_theme.sh
+++ dotfiles/.local/bin/switch_theme.sh
@@ -5,9 +5,15 @@ THEME_DIR="$HOME/nixos/themes/$THEME"
 if [ -d "$THEME_DIR" ]; then
     echo "$THEME" > ~/nixos/current_theme
 
-    ln -sf "$THEME_DIR/current-theme.conf" ~/.config/kitty/current-theme.conf
+    ln -sf "$THEME_DIR/kittyCol.conf" ~/.config/kitty/current-theme.conf
     ln -sf "$THEME_DIR/Colours.qml" ~/.config/quickshell/services/Colours.qml
-    ln -sf "$THEME_DIR/wallpapers" ~/Pictures/wallpapers
+    ln -sf "$THEME_DIR/wallpapers" ~/Pictures/
+
+    file=$(ls $THEME_DIR/wallpapers | shuf -n 1)
+
+    echo "$file selected"
+
+    swww img "$THEME_DIR/wallpapers/$file"
 
     kill -USR1 $(pgrep kitty)
 else
---
+++ dotfiles/.local/bin/theme_switcher.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+
+BASE_DIR="$HOME/nixos/themes"
+
+# Script to run when a folder is selected
+TARGET_SCRIPT="$HOME/.local/bin/switch_theme.sh"
+
+# Generate list of folders
+folders=()
+while IFS= read -r -d $'\0' dir; do
+    folders+=("$(basename "$dir")")
+done < <(find "$BASE_DIR" -mindepth 1 -maxdepth 1 -type d -print0)
+
+selected=$(printf '%s\n' "${folders[@]}" | rofi -dmenu -i -p "Select folder")
+
+if [[ -n "$selected" ]]; then
+    "$TARGET_SCRIPT" "$selected"
+    echo "Switched to theme: $selected"
+fi
