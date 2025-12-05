1434cfa95101c66e5ccaf21bddc8e4449e0cde8b
frittesauce <181068943+frittesauce@users.noreply.github.com>
2025-12-05 11:26:03 +0100

    blur is so sexy rn

--- modules/system/desktop/niri.nix
+++ modules/system/desktop/niri.nix
@@ -2,4 +2,30 @@
 
 {
   programs.niri.enable = true;
+
+  xdg.portal = {
+    enable = true;
+    wlr.enable = true;
+    extraPortals = with pkgs; [
+      xdg-dbus-proxy
+      xdg-desktop-portal
+      xdg-desktop-portal-gtk
+      xdg-desktop-portal-gnome
+    ];
+  };
+
+  environment.systemPackages = with pkgs; [
+    wl-clipboard
+    gnome-keyring
+    hyprlock
+    swww
+    xwayland-run
+    xwayland-satellite
+  ];
+
+  location = {
+    provider = "manual";
+    latitude = 38.8977;
+    longitude = 77.0365;
+  };
 }
